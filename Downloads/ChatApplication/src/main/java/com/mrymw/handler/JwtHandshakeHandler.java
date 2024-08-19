47ff297e6115fbc41321c94d8f1dd117e0384cc5
Maryam <mrymcodes@gmail.com>
2024-08-19 19:14:16 +0300

    added dependencies

--- Downloads/ChatApplication/src/main/java/com/mrymw/handler/JwtHandshakeHandler.java
+++ Downloads/ChatApplication/src/main/java/com/mrymw/handler/JwtHandshakeHandler.java
@@ -1,8 +1,11 @@
 package com.mrymw.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
@@ -10,9 +13,19 @@ import java.security.Principal;
 import java.util.Map;
 
 public class JwtHandshakeHandler extends DefaultHandshakeHandler {
+    public JwtHandshakeHandler(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
     protected Principal determineUser(HttpServletRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
+        String token = request.getParameter("token");
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            UserDetails userDetails = jwtTokenProvider.getUserDetails(token);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             return authentication;
         }
         return null;
